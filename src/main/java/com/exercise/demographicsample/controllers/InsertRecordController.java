5905a079d2f3ce42041007e0120319aec5bc5df3
Federico BERGAMO <bergamo.federico@gmail.com>
2017-08-16 21:36:59 +0200

    added thymeleaf dependency index and insert page added + bootstrap

---
+++ src/main/java/com/exercise/demographicsample/controllers/InsertRecordController.java
@@ -0,0 +1,17 @@
+package com.exercise.demographicsample.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class InsertRecordController {
+
+    @RequestMapping("/insert")
+    public String index(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "insert";
+    }
+
+}
