94bf774dd18f07af66d9b5221ca544ce706085f2
Federico Bergamo <bergamo.federico@gmail.com>
2017-08-16 19:14:24 +0200

    first commit

---
+++ src/main/java/com/exercise/demographicsample/DemographicSampleApplication.java
@@ -0,0 +1,12 @@
+package com.exercise.demographicsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemographicSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemographicSampleApplication.class, args);
+	}
+}
